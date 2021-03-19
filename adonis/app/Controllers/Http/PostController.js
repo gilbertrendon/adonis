6c2b66936999cc73c26dede042e17326b5d68549
root <jilberlv@hotmail.com>
2021-03-19 15:36:55 -0500

    find post by slug

--- adonis/app/Controllers/Http/PostController.js
+++ adonis/app/Controllers/Http/PostController.js
@@ -10,7 +10,23 @@ const Post = use('App/Models/Post')
 
 class PostController {
     async index({request, response}){
-        const {_limit, _start} = request.all()
+        const {_limit, _start, slug} = request.all()
+        if(slug){
+            const post = await Post
+            .query()
+            .with('user')
+            .with('categories')
+            .where('slug',slug)
+            .first()
+            
+            if(post){
+                return post
+            }
+            return response
+              .status(400)
+              .send({message: {e:'Post not found!'} })
+
+        }
         const posts= await Post
         .query()
         .with('user')
